3a438891fe32abb3ae553c99f55a11295c43d718
Fachreza Muslim <fachreza.muslim7396@gmail.com>
2022-10-03 03:39:19 +0800

    update conver function and adjust deploy hosting build

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,19 @@
 import * as functions from "firebase-functions"
 
 export const convertISBNNumberToURL = functions.https.onCall((data, context) => {
+    console.log('data', data)
     if (!data.isbn) return 'ISBN number is required';
 
     const validSize = ['S', 'M', 'L']
-    if (data.imgSize && !validSize.includes(imgSize)) {
+    if (data.imgSize && !validSize.includes(data.imgSize)) {
         return 'Invalid image size.'
     }
 
     const usedImgSize = data.imgSize || 'S'
+    const link = `https://covers.openlibrary.org/b/isbn/${data.isbn}-${usedImgSize}.jpg`
     return {
         isbn: data.isbn,
         imgSize: usedImgSize,
-        link: `https://covers.openlibrary.org/b/isbn/${data.isbn}-${usedImgSize}`
+        link
     }
 })
